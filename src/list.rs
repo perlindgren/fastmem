d8090a9bbd1dbc3dbdd18cd0de1687a3664a632f
Per Lindgren <per.lindgren@ltu.se>
2022-03-25 23:09:55 +0100

    wip list works

--- src/list.rs
+++ src/list.rs
@@ -1,71 +1,118 @@
 use core::fmt;
+use core::ptr::NonNull;
 
-#[derive(Copy, Clone)]
+#[derive(Copy, Clone, Debug)]
 pub struct Node {
     data: usize,
-    next: Option<&'static Node>,
+    next: Option<NonNull<Node>>,
 }
 
-pub struct List {
-    head: usize,
-}
-
-impl List {
-    // this works for just push one node to the top
-    pub fn push(&mut self, &) -> List {
-        List::Node(self, m)
-    }
-
-    pub fn pop(&mut self) -> &'static List {
-        match self {
-            List::Node(next, data) => (*List::to_list(*next), data),
-            _ => panic!(),
-        }
-    }
-
-    pub fn to_raw(&self) -> *const List {
-        &*self
-    }
-
-    pub fn to_list(l: *const List<'a>) -> &List<'a> {
-        unsafe { &*(l as *const List<'a>) }
-    }
-}
-
-impl<'a> fmt::Debug for List<'a> {
+impl fmt::Display for Node {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(
             f,
-            "{}",
-            match self {
-                List::Node(l, m) => format!("Node ({:?}, {:?})", m, List::to_list(*l)),
-                _ => "Null".to_string(),
+            "({}, {})",
+            self.data,
+            match self.next {
+                Some(node) => {
+                    let node = unsafe { node.as_ref() };
+                    format!("{}", node)
+                }
+                None => "None".to_string(),
             }
         )
+    }
+}
+
+#[derive(Copy, Clone, Debug)]
+pub struct Stack {
+    head: Option<NonNull<Node>>,
+}
+
+impl fmt::Display for Stack {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "Stack({})",
+            match self.head {
+                Some(node) => {
+                    let node = unsafe { node.as_ref() };
+                    format!("{}", node)
+                }
+                None => "None".to_string(),
+            }
+        )
+    }
+}
+
+impl Stack {
+    pub const fn new() -> Self {
+        Stack { head: None }
+    }
+    // this works for just push one node to the top
+    pub fn push(&mut self, n: &mut Node) {
+        n.next = self.head;
+        self.head = NonNull::new(n)
+    }
+
+    pub fn pop(&mut self) -> Option<&mut Node> {
+        match self.head {
+            Some(mut node) => {
+                let node = unsafe { node.as_mut() };
+                self.head = node.next;
+
+                Some(node)
+            }
+            None => None,
+        }
     }
 }
 
 #[cfg(test)]
 mod test {
     use super::*;
-    use core::mem::size_of_val;
+    use core::mem::{size_of, size_of_val};
+
+    #[test]
+    fn test_size_list() {
+        let l = Stack::new();
+        assert_eq!(size_of_val(&l), 8);
+        assert_eq!(size_of::<Stack>(), 8)
+    }
 
     #[test]
     fn test_list() {
-        let l = List::Null;
-        println!("l {:?}", l);
+        let mut l = Stack::new();
+        println!("l {}", l);
+        let mut n = Node {
+            data: 0,
+            next: None,
+        };
 
-        let l = l.push(&[1]);
-        println!("l {:?}", l);
+        l.push(&mut n);
+        println!("l {}", l);
+        println!("n {}", n);
 
-        let l = l.push(&[2]);
-        println!("l {:?}", l);
+        let mut n2 = Node {
+            data: 1,
+            next: None,
+        };
 
-        let (l, d) = l.pop();
-        println!("l {:?}, d {:?}", l, d);
+        l.push(&mut n2);
 
-        let (l, d) = l.pop();
-        println!("l {:?}, d {:?}", l, d);
+        println!("l {}", l);
+
+        let n = l.pop().unwrap();
+        println!("n {}", n);
+        println!("l {}", l);
+
+        let n = l.pop().unwrap();
+        println!("n {}", n);
+        println!("l {}", l);
+
+        let n = l.pop();
+        println!("n {:?}", n);
+        println!("l {}", l);
     }
 
     // #[test]
